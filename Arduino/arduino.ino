cd77c0f3520f6b73377e53bc9be41f0d29dd23a4
ceniceroscarlos0001 <123529485+ceniceroscarlos0001@users.noreply.github.com>
2025-05-19 10:01:38 -0600

    Correcion

--- Arduino/arduino.ino
+++ Arduino/arduino.ino
@@ -2,27 +2,24 @@ const int pines[8] = {2, 3, 4, 5, 6, 7 ,8, 9};
 int i;
 
 void setup() {
-  Serial.begin(9600);  // Inicia comunicación serial
+  Serial.begin(9600);
   for (i = 0; i <= 7; i++) {
-    pinMode(pines[i], INPUT);
+    pinMode(pines[i], INPUT_PULLUP);  // entradas con pull-up interno activado
   }
 }
 
 void loop() {
   String binario = "";
-  
+
   for (i = 0; i <= 7; i++) {
-    int valore = digitalRead(pines[i]);
+    int valore = !digitalRead(pines[i]);  // invertimos porque pull-up da HIGH por defecto
     binario += String(valore);
   }
 
-  // Obtenemos el tiempo desde que Arduino fue encendido
   unsigned long tiempo = millis();
-
-  // Enviar una sola línea con binario y timestamp separados por coma
   Serial.print(binario);
   Serial.print(",");
   Serial.println(tiempo);
 
-  delay(100);  // Espera para evitar saturación
+  delay(100);
 }
